ba360c57974b19d56d6f84216763aad0888ad16d
Amelie Boudier <amelie.boudier01@gmail.com>
2020-02-19 11:05:24 +0100

    rails new

--- Gemfile
+++ Gemfile
@@ -1,10 +1,10 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.4.4'
+ruby '2.5.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.2.1'
+gem 'rails', '~> 5.2.2'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
@@ -13,6 +13,8 @@ gem 'puma', '~> 3.11'
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
+# Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
+gem 'webpacker'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
 
@@ -35,13 +37,6 @@ gem 'jbuilder', '~> 2.5'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
-
-gem 'bootstrap-sass'
-gem 'font-awesome-sass', '~> 5.0.9'
-gem 'simple_form'
-gem 'autoprefixer-rails'
-gem 'jquery-rails'
-gem 'faker'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -67,5 +62,3 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-gem 'rspec-rails', group: [ :test ]
-gem 'rails-controller-testing', group: [ :test ]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,47 +1,7 @@
-puts 'Destroying all instances of restaurants'
-Restaurant.destroy_all
-
-puts 'Creating new instances'
-
-restaurant_attr = [
-  {
-    name: 'Tim Raue',
-    address: 'Rudi-Dutschke-Str. - Berlin',
-    phone_number: '0176 25515236',
-    category: 'japanese'
-  },
-  {
-    name: 'Mutti Kreuzberg',
-    address: 'Bergmanstr. - Berlin',
-    phone_number: '0176 15234856',
-    category: 'belgian'
-  },
-  {
-    name: 'La Bionda',
-    address: 'Zossenerstr. - Berlin',
-    phone_number: '0176 42563218',
-    category: 'italian'
-  },
-  {
-    name: 'Beijing Duck',
-    address: 'Zwinglistr. - Berlin',
-    phone_number: '0176 46529713',
-    category: 'chinese'
-  },
-  {
-    name: 'Madame Claude',
-    address: 'Kantstr. - Berlin',
-    phone_number: '0176 91732468',
-    category: 'french'
-  },
-  {
-    name: 'Sushi Lust',
-    address: 'Torstr. - Berlin',
-    phone_number: '0176 37942685',
-    category: 'japanese'
-  }
-]
-
-Restaurant.create!(restaurant_attr)
-
-puts 'Seed done!'
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
